dfa9827d5ab0f73a57a7d43c4ab73251208df795
moseiei132 <moseiei132@gmail.com>
2025-12-05 13:51:41 +0700

    Initial Commit

---
+++ QuIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuIt
+//
+//  Created by Dulyawat on 5/12/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuIt/QuItApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuItApp.swift
+//  QuIt
+//
+//  Created by Dulyawat on 5/12/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct QuItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
